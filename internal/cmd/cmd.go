bdbad1f510ac79d6234f99d946e100e5563439a8
Tom Fleet <tomfleet2018@gmail.com>
2025-03-29 13:52:22 +0000

    Add a logger and verbose flags (#4)

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -26,13 +26,15 @@ func Build() (*cli.Command, error) {
 
 // check returns the check subcommand.
 func check() (*cli.Command, error) {
+	var options dead.CheckOptions
 	return cli.New(
 		"check",
 		cli.Short("Check a file or files in a directory (recursively) for dead links"),
 		cli.RequiredArg("path", "Path to the file or directory to scan"),
+		cli.Flag(&options.Debug, "debug", cli.NoShortHand, false, "Enable debug logging"),
 		cli.Run(func(cmd *cli.Command, args []string) error {
-			dead := dead.New(cmd.Stdout(), cmd.Stderr())
-			return dead.Check(cmd.Arg("path"))
+			dead := dead.New(cmd.Stdout(), cmd.Stderr(), options.Debug)
+			return dead.Check(cmd.Arg("path"), options)
 		}),
 	)
 }
--- internal/dead/dead.go
+++ internal/dead/dead.go
@@ -4,21 +4,85 @@ package dead
 import (
 	"fmt"
 	"io"
+	"strings"
+
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/log"
 )
 
 // Dead holds the configuration and state of the dead program.
 type Dead struct {
-	stdout io.Writer // Normal program output is written here
-	stderr io.Writer // Logs, debug info and errors go here
+	stdout io.Writer   // Normal program output is written here
+	stderr io.Writer   // Logs, debug info and errors go here
+	logger *log.Logger // The global logger
 }
 
 // New returns a new instance of [Dead].
-func New(stdout, stderr io.Writer) Dead {
-	return Dead{stdout: stdout, stderr: stderr}
+func New(stdout, stderr io.Writer, debug bool) Dead {
+	const width = 5
+
+	level := log.InfoLevel
+	if debug {
+		level = log.DebugLevel
+	}
+
+	logger := log.NewWithOptions(stderr, log.Options{
+		ReportTimestamp: true,
+		Level:           level,
+	})
+
+	// Largely the default styles but with a default MaxWidth of 5 so as to not cutoff
+	// DEBUG or ERROR
+	logger.SetStyles(&log.Styles{
+		Timestamp: lipgloss.NewStyle(),
+		Caller:    lipgloss.NewStyle().Faint(true),
+		Prefix:    lipgloss.NewStyle().Bold(true).Faint(true),
+		Message:   lipgloss.NewStyle(),
+		Key:       lipgloss.NewStyle().Faint(true),
+		Value:     lipgloss.NewStyle(),
+		Separator: lipgloss.NewStyle().Faint(true),
+		Levels: map[log.Level]lipgloss.Style{
+			log.DebugLevel: lipgloss.NewStyle().
+				SetString(strings.ToUpper(log.DebugLevel.String())).
+				Bold(true).
+				MaxWidth(width).
+				Foreground(lipgloss.Color("63")),
+			log.InfoLevel: lipgloss.NewStyle().
+				SetString(strings.ToUpper(log.InfoLevel.String())).
+				Bold(true).
+				MaxWidth(width).
+				Foreground(lipgloss.Color("86")),
+			log.WarnLevel: lipgloss.NewStyle().
+				SetString(strings.ToUpper(log.WarnLevel.String())).
+				Bold(true).
+				MaxWidth(width).
+				Foreground(lipgloss.Color("192")),
+			log.ErrorLevel: lipgloss.NewStyle().
+				SetString(strings.ToUpper(log.ErrorLevel.String())).
+				Bold(true).
+				MaxWidth(width).
+				Foreground(lipgloss.Color("204")),
+			log.FatalLevel: lipgloss.NewStyle().
+				SetString(strings.ToUpper(log.FatalLevel.String())).
+				Bold(true).
+				MaxWidth(width).
+				Foreground(lipgloss.Color("134")),
+		},
+		Keys:   map[string]lipgloss.Style{},
+		Values: map[string]lipgloss.Style{},
+	})
+
+	return Dead{stdout: stdout, stderr: stderr, logger: logger}
+}
+
+// CheckOptions are the flags passed to the check subcommand.
+type CheckOptions struct {
+	Debug bool // Enable verbose logging
 }
 
 // Check is the entry point for the `dead check` subcommand.
-func (d Dead) Check(path string) error {
+func (d Dead) Check(path string, options CheckOptions) error {
+	d.logger.Debug("Checking links", "path", path)
 	fmt.Fprintf(d.stdout, "Checking %s\n", path)
 	return nil
 }
