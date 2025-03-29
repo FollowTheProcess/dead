dbe459b23fcc5421d0ab2aac43e4069b63e2b001
Tom Fleet <tomfleet2018@gmail.com>
2025-03-29 16:26:39 +0000

    Actually do the HTTP requests

--- cmd/dead/main_test.go
+++ cmd/dead/main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
 	"testing"
 
 	"github.com/rogpeppe/go-internal/testscript"
@@ -25,5 +26,43 @@ func Test(t *testing.T) {
 		UpdateScripts:       false,
 		RequireExplicitExec: true,
 		RequireUniqueNames:  true,
+		Cmds: map[string]func(ts *testscript.TestScript, neg bool, args []string){
+			"replace": Replace,
+		},
 	})
+}
+
+// Replace is a testscript command that replaces text in a file by way of a regex
+// pattern match, useful for replacing non-deterministic output like UUIDs and durations
+// with placeholders to facilitate deterministic comparison in tests.
+//
+// Usage:
+//
+//	replace <file> <regex> <replacement>
+//
+// It cannot be negated, regex must be valid, and the file must be present in the
+// txtar archive, including "stdout" and "stderr".
+func Replace(ts *testscript.TestScript, neg bool, args []string) {
+	if neg {
+		ts.Fatalf("unsupported: ! filter")
+	}
+
+	if len(args) != 3 {
+		ts.Fatalf("Usage: filter <file> <regex> <replacement>")
+	}
+
+	file := ts.MkAbs(args[0])
+	ts.Logf("filter file: %s", file)
+
+	stdout := ts.ReadFile("stdout")
+	regex := args[1]
+	replacement := args[2]
+
+	re, err := regexp.Compile(regex)
+	ts.Check(err)
+
+	replaced := re.ReplaceAllString(stdout, replacement)
+
+	_, err = ts.Stdout().Write([]byte(replaced))
+	ts.Check(err)
 }
--- internal/dead/dead.go
+++ internal/dead/dead.go
@@ -2,12 +2,36 @@
 package dead
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"io"
+	"net/http"
+	"os"
 	"strings"
+	"text/tabwriter"
+	"time"
 
+	"github.com/FollowTheProcess/dead/internal/extractor"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/log"
+)
+
+const (
+	// DefaultRequestTimeout is the default value for the per-request timeout.
+	DefaultRequestTimeout = 5 * time.Second
+
+	// for checking all detected links.
+	DefaultOverallTimeout = 30 * time.Second
+)
+
+// TabWriter config.
+const (
+	minWidth = 1   // Min cell width
+	tabWidth = 8   // Tab width in spaces
+	padding  = 2   // Padding
+	padChar  = ' ' // Char to pad with
+	flags    = 0   // Config flags
 )
 
 // Dead holds the configuration and state of the dead program.
@@ -77,12 +101,115 @@ func New(stdout, stderr io.Writer, debug bool) Dead {
 
 // CheckOptions are the flags passed to the check subcommand.
 type CheckOptions struct {
-	Debug bool // Enable verbose logging
+	Debug          bool          // Enable verbose logging
+	RequestTimeout time.Duration // Per request timeout
+	Timeout        time.Duration // Timeout for the whole operation
+}
+
+// CheckResult holds the result of a link check.
+type CheckResult struct {
+	Err        error
+	URL        string
+	Status     string
+	StatusCode int
+	Duration   time.Duration
 }
 
 // Check is the entry point for the `dead check` subcommand.
 func (d Dead) Check(path string, options CheckOptions) error {
+	ctx, cancel := context.WithTimeout(context.Background(), options.Timeout)
+	defer cancel()
+
+	// See if path is a file or a directory
+	// File: Detect the type and use the right extractor, then extract the links
+	// Dir: Traverse it recursively, extract links from all files until we've hit the bottom
+	// probs concurrently in a pipeline across goroutines
+
+	// The we have a load of links, fan them out over a load of goroutines in a pipeline
+	// doing a http get in each
+
+	// Range over the results channel which should return the URL, status code and the message
+	// if it's not ok. But not stop the loop, we need to keep processing all links
 	d.logger.Debug("Checking links", "path", path)
-	fmt.Fprintf(d.stdout, "Checking %s\n", path)
+
+	info, err := os.Stat(path)
+	if err != nil {
+		return fmt.Errorf("os.Stat(%s): %w", path, err)
+	}
+
+	if info.Mode().IsDir() {
+		return errors.New("TODO: Support recursing a directory searching for links")
+	}
+
+	if !info.Mode().IsRegular() {
+		return fmt.Errorf("%s has unsupported file mode: %s", path, info.Mode())
+	}
+
+	// Now we know it's a regular file
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	// TODO(@FollowTheProcess): This is all synchronous while I polish it and ensure it works
+	// once I have good test coverage and the UX I want, let's make it fan out in a pipeline
+
+	ext := extractor.NewText(f)
+	links, err := ext.Extract()
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{
+		Timeout: options.RequestTimeout,
+	}
+
+	requests := make([]*http.Request, 0, len(links))
+	for _, link := range links {
+		request, err := http.NewRequestWithContext(ctx, http.MethodGet, link, nil)
+		if err != nil {
+			return err
+		}
+		// TODO(@FollowTheProcess): Set User-Agent
+		requests = append(requests, request)
+	}
+
+	results := make([]CheckResult, 0, len(links))
+	for _, request := range requests {
+		var result CheckResult
+		start := time.Now()
+		response, err := client.Do(request)
+		if err != nil {
+			result = CheckResult{
+				URL:      request.URL.String(),
+				Err:      err,
+				Duration: time.Since(start),
+			}
+		} else {
+			result = CheckResult{
+				URL:        request.URL.String(),
+				StatusCode: response.StatusCode,
+				Status:     response.Status,
+				Err:        err,
+				Duration:   time.Since(start),
+			}
+			response.Body.Close()
+		}
+
+		results = append(results, result)
+	}
+
+	tw := tabwriter.NewWriter(d.stdout, minWidth, tabWidth, padding, padChar, flags)
+	for _, result := range results {
+		if result.Err != nil {
+			fmt.Fprintf(tw, "%s\t%v\t%s\n", result.URL, result.Err, result.Duration)
+		} else {
+			fmt.Fprintf(tw, "%s\t%s\t%s\n", result.URL, result.Status, result.Duration)
+		}
+	}
+
+	tw.Flush()
+
 	return nil
 }
